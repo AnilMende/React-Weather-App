3424af8c01c788c9e70b671669255a699854864a
Anil <anilmende2002@email.com>
2025-09-01 02:14:20 +0530

    v4

--- src/forecast/forecast.js
+++ src/forecast/forecast.js
@@ -26,7 +26,7 @@ const Forecast = ({ data }) => {
 
     return (
         <>
-            <label className="title">Daily</label>
+            <label className="title">Next 7-Days Forecast</label>
             <Accordion allowZeroExpanded>
                 {data.list.splice(0, 7).map((item, indx) => (
                     <AccordionItem key={indx}>
@@ -35,10 +35,11 @@ const Forecast = ({ data }) => {
                             <AccordionItemButton>
                                 <div className="daily-item">
                                     <img alt="weather" 
-                                    className="icon-small" 
-                                    src={`icons/${item.weather[0].icon}.png`} />
+                                         className="icon-small" 
+                                         src={`icons/${item.weather[0].icon}.png`} 
+                                    />
 
-                                    <label className="Day">{forecastDays[indx]}</label>
+                                    <label className="day">{forecastDays[indx]}</label>
                                     <label className="description">{item.weather[0].description}</label>
                                     <label className="min-max">{Math.round(item.main.temp_min)}°C / 
                                          {Math.round(item.main.temp_max)}°C
@@ -48,7 +49,40 @@ const Forecast = ({ data }) => {
                             </AccordionItemButton>
                         </AccordionItemHeading>
 
-                        <AccordionItemPanel></AccordionItemPanel>
+                        <AccordionItemPanel>
+                            <div className="daily-details-grid">
+                                <div className="daily-details-grid-item">
+                                    <label>Pressure:</label>
+                                    <label>{item.main.pressure} hPa</label>
+                                </div>
+
+                                <div className="daily-details-grid-item">
+                                    <label>Humidity:</label>
+                                    <label>{item.main.humidity}%</label>
+                                </div>
+
+                                <div className="daily-details-grid-item">
+                                    <label>Clouds:</label>
+                                    <label>{item.clouds.all}%</label>
+                                </div>
+
+                                <div className="daily-details-grid-item">
+                                    <label>Wind Speed:</label>
+                                    <label>{item.wind.speed} m/s</label>
+                                </div>
+
+                                <div className="daily-details-grid-item">
+                                    <label>Sea Level:</label>
+                                    <label>{item.main.sea_level}m</label>
+                                </div>
+
+                                <div className="daily-details-grid-item">
+                                    <label>Feels Like:</label>
+                                    <label>{Math.round(item.main.feels_like)}°C</label>
+                                </div>
+                            </div>
+
+                        </AccordionItemPanel>
                     </AccordionItem>
                 ))}
             </Accordion>
